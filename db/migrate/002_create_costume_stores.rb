6403ff68d644c4fc89291a9ba6569b812dc6fb41
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2018-10-18 00:59:58 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,6 +3,9 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
     def change 
     create_table :costume_stores do |t|
     t.string :name
+    t.string :location
+    t.integer :costume_inventory
+    t.integer :number_of_employees
     t.float :price
     t.size :string
     t.image_url :string 
