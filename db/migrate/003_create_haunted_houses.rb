72689c2ce65975b82b3abdf70c01351b530f5b9e
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2018-10-18 01:09:48 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,6 +1,17 @@
 class CreateHauntedHouses < ActiveRecord::Migration[5.1]
   
-  
+  def change 
+    create_table :haunted_houses do |t|
+    t.string :name
+    t.string :location
+    t.string :theme
+    t.float :price
+    t.boolean :
+    t.datetime :opening_time
+    t.datetime :closing_time 
+    
+    t.timestamps
+    end
   
 end 
 
